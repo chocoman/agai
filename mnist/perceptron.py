d9932b31eac97321e9ca0ab843c43de43748089a
eleniel-mocna <soukup.sam@gmail.com>
2019-03-18 16:18:27 +0100

    implemented learning rate to perceptron

--- mnist/perceptron.py
+++ mnist/perceptron.py
@@ -19,35 +19,39 @@ class perceptron_mnist:
         return guess
 
 
-    def train(self, get_features, nepochs):
+    def train(self, get_features, nepochs,meta_epoch = (10,1,0.1,0.1,0.1)):
         self.nfeatures = len(get_features(self.trX[0]))
         # self.w[f, c] is score for class c if feature f is detected
         self.w = np.zeros((self.nfeatures, self.NCLASSES))
         nsamples = self.trX.shape[0]
+        #Adjusting the learning rate every 8 epochs
         all_features = np.zeros((nsamples, self.nfeatures))
         print('recognizing features...')
         for i in range(nsamples):
             all_features[i,:] = get_features(self.trX[i])
         print('features recognized')
-        for epoch in range(nepochs):
-            nerrors = 0
-            print('training epoch ' + str(epoch))
-            for i in range(nsamples):                
-                image_class = self.trY[i]
-                # features = get_features(self.trX[i])
-                features = all_features[i,:]
-                predicted_class = self.classify(features)
-                if predicted_class != image_class:
-                    nerrors += 1
-                    # self.w[:,image_class] zvysit tam kde jsou jednicky
-                    self.w[:,image_class] += features * self.learning_rate
-                    # self.w[:,predicted_class] snizit tam kde jsou jednicky
-                    self.w[:,predicted_class] -= features * self.learning_rate
-                    # TODO update weights to predict better next time\
-                if (i % 200 == 0):
-                    sys.stdout.flush()
-                    sys.stdout.write(' image {} error {}\r'.format(i, nerrors/(i+1)))
-            print('training errors: ' + str(nerrors/nsamples))
+        for k in range(nepochs):
+            self.learning_rate = meta_epoch[k]
+            self.test(get_features)
+            print ("Learning rate now set to " + str(self.learning_rate))
+            for epoch in range(nepochs):
+                nerrors = 0            
+                for i in range(nsamples):                
+                    image_class = self.trY[i]
+                    # features = get_features(self.trX[i])
+                    features = all_features[i,:]
+                    predicted_class = self.classify(features)
+                    if predicted_class != image_class:
+                        nerrors += 1
+                        # self.w[:,image_class] zvysit tam kde jsou jednicky
+                        self.w[:,image_class] += features * self.learning_rate
+                        # self.w[:,predicted_class] snizit tam kde jsou jednicky
+                        self.w[:,predicted_class] -= features * self.learning_rate
+                        # TODO update weights to predict better next time\
+                    if (i % 200 == 0):
+                        sys.stdout.flush()
+                        sys.stdout.write(' image {} error {}\r'.format(i, nerrors/(i+1)))
+                print('training errors: ' + str(nerrors/nsamples))
         
 
     def test(self, get_features):
